8bad81e459e0f2e5b48228edb05cb51db19d75eb
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-13 15:08:55 +0100

    Die Schleife anpassen, richtige Daten aus Store rasholen.

--- src/components/CalendarWeek.vue
+++ src/components/CalendarWeek.vue
@@ -2,7 +2,7 @@
   <div id="calender-week">
     <div class="card-group">
       <!-- Anfang: Template für die Calendar-Day-Component -->
-      <CalendarDay v-for="i in 7 " :key="i" />
+      <CalendarDay v-for="day in calendarWeekData " :key="day.id" />
       <!-- Ende: Template für die Calendar-Day-Component -->
     </div>
   </div>
