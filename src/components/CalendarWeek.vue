6013bc8b7cf87b3383c618f487e3c3c9d60b40d6
Eddyhotkey <rshdbairamov@gmail.com>
2026-03-13 14:56:53 +0100

    Die Templates wurden angepasst.Fehler Behebung.

--- src/components/CalendarWeek.vue
+++ src/components/CalendarWeek.vue
@@ -1,17 +1,9 @@
 <template>
   <div id="calender-week">
     <div class="card-group">
-      <div class="card" v-for="i in 7" :key="i">
-        <div class="card-header text-center" role="button">
-          <strong>Montag</strong>
-        </div>
-        <div class="card-body">
-          <!-- Anfang: Template für die Calendar-Day-Component -->
-          <CalendarDay />
-
-          <!-- Ende: Template für die Calendar-Day-Component -->
-        </div>
-      </div>
+      <!-- Anfang: Template für die Calendar-Day-Component -->
+      <CalendarDay v-for="i in 7 " :key="i" />
+      <!-- Ende: Template für die Calendar-Day-Component -->
     </div>
   </div>
 </template>
